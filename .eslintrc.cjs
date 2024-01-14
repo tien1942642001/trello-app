996870a579ce07928a7e0d6816e6a327e846871b
vu van tien <tien194264@huce.edu.vn>
2024-01-14 14:07:12 +0700

    update base css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
     'space-before-blocks': ['error', 'always'],
     'object-curly-spacing': [1, 'always'],
     'indent': ['warn', 2],
-    'semi': [1, 'always'],
+    'semi': [1, 'never'],
     'quotes': ['error', 'single'],
     'array-bracket-spacing': 1,
     'linebreak-style': 0,
