c2a2fb93c0d3611f480747eb4a9050d3b0c0a76a
vu van tien <tien194264@huce.edu.vn>
2024-01-14 21:58:30 +0700

    dark_mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
+import ButtonComponent from './components/ButtonComponent'
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <>
+      <Button onClick={() => {
+        setMode(mode === 'dark' ? 'light' : 'dark')
+      }}>{mode === 'dark' ? 'light' : 'dark'}</Button>
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-      <p>Hello world</p>
-      <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
-      <br />
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-
-      <HomeIcon />
-      <HomeIcon color='primary' />
-      <HomeIcon color='secondary' />
-      <HomeIcon color='success' />
-      <HomeIcon color='action' />
-      <HomeIcon color='disabled' />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <ModeToggle />
+      <ButtonComponent />
     </>
   )
 }
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button'
+
+export default function ButtonComponent() {
+  return (
+    <div>
+      <Button variant='contained'>Click me</Button>
+    </div>
+  )
+}
