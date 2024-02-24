03f7f99d60ed8e8ab9a4f1d22bc94d8b54862339
vu van tien <tien194264@huce.edu.vn>
2024-02-24 16:17:31 +0700

    refactor structor project

---
+++ src/components/AppBar/Menus/Starred.jsx
@@ -0,0 +1,46 @@
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Starred() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Button
+                id="basic-button-starred"
+                aria-controls={open ? 'basic-menu-starred' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                endIcon={<ExpandMoreIcon/>}
+            >
+        Starred
+            </Button>
+            <Menu
+                id="basic-menu-starred"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button-starred'
+                }}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
+export default Starred;
